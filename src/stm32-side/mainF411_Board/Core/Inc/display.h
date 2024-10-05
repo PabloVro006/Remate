faced1312e5cbbe795b560553bcc06273dfd4ce6
PabloVro006 <vairop005@gmail.com>
2024-10-05 17:53:32 +0200

    coding starts

---
+++ src/stm32-side/mainF411_Board/Core/Inc/display.h
@@ -0,0 +1,8 @@
+#ifndef INC_DISPLAY_H_
+#define INC_DISPLAY_H_
+#include "config.h"
+
+void startDisplay();
+void printBufferData(uint8_t buffer);
+
+#endif /* INC_DISPLAY_H_ */
---
+++ src/stm32-side/mainF411_Board/Core/Inc/spi_handler.h
@@ -0,0 +1,10 @@
+#ifndef INC_SPI_HANDLER_H_
+#define INC_SPI_HANDLER_H_
+/*
+extern uint8_t spiRxBuffer;
+extern uint8_t spiTxBuffer;
+
+static uint8_t receiveFromPi();
+static uint8_t sendToPi();
+*/
+#endif /* INC_SPI_HANDLER_H_ */
