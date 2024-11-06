793b4722030394b646bf96e2a3ab40e99fd00dad
PabloVro006 <vairop005@gmail.com>
2024-11-06 16:28:49 +0100

    commenting mainTransistor.ino code

--- src/arduino-side/mainTransistor/mainTransistor.ino
+++ src/arduino-side/mainTransistor/mainTransistor.ino
@@ -17,7 +17,7 @@ enum TrashType {
   TRASH_NONE = 0,
   TRASH_METAL = 1,
   TRASH_PLASTIC = 2,
-  TRASH_INCOMING = 9,
+  TRASH_INCOMING = 9,  // 9 is a flag indicating that something has been detected form the Rpi4, but it's not defined yet
 };
 
 // HALL SETUP
@@ -111,40 +111,47 @@ void setup() {
 
 // LOOP
 void loop() {
-  // Get current time
-  unsigned long currentMillis = millis();
-  //unsigned long intervalToConsider = paddleMotorStruct.going ? paddleGoingInterval : paddleNotGoingInterval;
+  unsigned long currentMillis = millis();  // Get current time
+  /*unsigned long intervalToConsider = paddleMotorStruct.going ? paddleGoingInterval : paddleNotGoingInterval;*/
+  // Checking if it's time to switch the going state of the paddle
   if (currentMillis - previousMillis >= (paddleMotorStruct.going ? paddleGoingInterval : paddleNotGoingInterval)) {
     previousMillis = currentMillis;
-    paddleMotorStruct.going = !paddleMotorStruct.going;
+    paddleMotorStruct.going = !paddleMotorStruct.going; // Change the going state
   }
 
-  // Get trash
+  // Get trash only if Remate is not throwing (motors aren't moving)
   if(!isThrowing){
     trash = getTrashFromPi();
+    // Condition for when the Rpi4 has sent something
     if(trash != TRASH_NONE){
+      // Stops the paddle
       paddleMotorStruct.power = 0;
       controlPaddleMotor(&paddleMotorStruct);
       if(trash == TRASH_INCOMING){
+        // Wait max 5 seconds for the defined trash
         unsigned long startMillis = millis();
         while(trash == TRASH_INCOMING || trash == TRASH_NONE){
           unsigned long currentMillisForTrashIncoming = millis();
           trash = getTrashFromPi();
           delay(serialDelay);
+          // Checking if the 5 seconds max has passed
           if(currentMillisForTrashIncoming - startMillis >= trashIncomingTimeout){
+            // Resets all the variable and restarts the loop()
             trash = TRASH_NONE;
             paddleMotorStruct.power = 1;
-            return; // Restarts the loop()
+            return;
           }
         };
       }
-      isThrowing = true;
+      isThrowing = true;  // This is useful for stopping the data reception form the Rpi4
       trash == TRASH_METAL ? throwTrash(trashTypeMetal) : throwTrash(trashTypePlastic);
+      // Send feedbakc to Rpi4 and start moving the paddle again
       sendFeedbackToPi(feedbackOk);
       paddleMotorStruct.power = 1;
       paddleMotorStruct.going = 0;
     }
   }
+  // Call the function for moving the paddle based on the parameters that as been changed
   controlPaddleMotor(&paddleMotorStruct);
 }
 
@@ -152,12 +159,17 @@ void loop() {
 // Hall reading function
 bool hallCheck(int hall) {
   delay(serialDelay);
-  int reading = analogRead(hall);
-  return (reading > hallThresholdLow && reading < hallThresholdHigh);
+  int reading = analogRead(hall);  // Read the output of the hall passed
+  return (reading > hallThresholdLow && reading < hallThresholdHigh);  // Check if a magnet is detected
 }
 
 // Turn off motors
+/*
+The parameter is a list containg all the motor's indexes that has to be turned off.
+The last value of the list is a -1 flag, indicating the end for the loop.
+*/
 void turnMotorsOff(const int motorIndexes[]){
+  // Setting to LOW all the transistor of the motors passed
 	for (int i=0; motorIndexes[i] != -1; i++){
     const MotorData& motor = motorData[motorIndexes[i]];
     digitalWrite(motor.CLOCK_NPN, LOW);
@@ -170,19 +182,26 @@ void turnMotorsOff(const int motorIndexes[]){
 // Paddle movement
 void controlPaddleMotor(PaddleMotorStruct* motorController) {
   if(motorController->power && motorController->going) {
-    digitalWrite(PADDLE_NPN, HIGH);
+    digitalWrite(PADDLE_NPN, HIGH);  // Activate the transistor and so make the paddle move
   }else{
-    digitalWrite(PADDLE_NPN, LOW);
+    digitalWrite(PADDLE_NPN, LOW);  // Setting the transistor to LOW and so make the paddle stop
   }
 }
 
 // Generic function to move motors
+/*
+The first parameter is the index of the motors to rotate (0 -> disk, 1 -> cross)
+The second one is the direction of the rotation (0 -> clockwise, 1 -> counterclockwise)
+The third one is the times that the motor must rotate (1 times -> 1 hall detections, 2 times -> 2 hall detections...)
+*/
 void rotateMotor(uint8_t motorIndex, uint8_t rotationDirection, uint8_t times) {
-  int motors[2] = {(int)motorIndex, -1};
+  int motors[2] = {(int)motorIndex, -1};  // Creating a list containg the motor and the -1 flag
+  // Move the motor away from the magnet or else the hall will detect it and the rotation won't be done
   for (int i = 0; i < times; i++) {
     digitalWrite(motorData[motorIndex].COUNTER_RELAY, rotationDirection);
     digitalWrite(motorData[motorIndex].CLOCK_RELAY, !rotationDirection);
     delay(rotationDelayMs);
+    // Now it can start rotating until a magnet is found from the hall
     while (hallCheck(motorData[motorIndex].HALL)) {
       digitalWrite(motorData[motorIndex].COUNTER_RELAY, rotationDirection);
       digitalWrite(motorData[motorIndex].CLOCK_RELAY, !rotationDirection);
@@ -194,8 +213,9 @@ void rotateMotor(uint8_t motorIndex, uint8_t rotationDirection, uint8_t times) {
 // Throw function
 void throwTrash(TrashType trashType){
   uint8_t motorIndex = (trashType == TRASH_METAL) ? 0 : 1;
-  uint8_t order = (trashType == TRASH_METAL) ? 1 : 0; // 0 for clockwise, 1 for counterclockwise
+  uint8_t order = (trashType == TRASH_METAL) ? 1 : 0;
   rotateMotor(motorIndex,order,1);
+  // If the motor moved is the cross there is no need to move it back again
   if(trashType == TRASH_METAL){
     rotateMotor(motorIndex,!order,1);
   }
@@ -205,21 +225,21 @@ void throwTrash(TrashType trashType){
 // RPI4 COMMUNICATION
 // Get trash from Rpi4
 int getTrashFromPi() {  
-  Serial.flush();
+  Serial.flush();  // Clear the outgoing buffer
   int trashGet = TRASH_NONE;
   if (Serial.available() > 0) {
-    // Get serial input
-    trashGet = Serial.parseInt();
+    trashGet = Serial.parseInt();  // Get serial input
     if ((trashGet < TRASH_NONE || trashGet > TRASH_PLASTIC) && trashGet != TRASH_INCOMING) {
       trashGet = TRASH_NONE;  // Reset to default if invalid
     }
   }
   return trashGet;  // Return serial input
 }
+
 // Send the feedback to Rpi4
 void sendFeedbackToPi(int feedbackNumber){
-  Serial.println(feedbackNumber);
-  Serial.flush();
+  Serial.println(feedbackNumber);  // Send the "done" flag to the Rpi4
+  Serial.flush();  // Clear the outgoing buffer
   delay(serialDelay);
   while (Serial.available() > 0) { // Clear any remaining data in the input buffer
     Serial.read();
