5edd13c85db5ec16478112740eb7458d29013490
Gyzmoooo <giuliogismondi06@gmail.com>
2024-11-19 11:53:52 +0100

    Cleaned and commented server.py

--- src/rpi4-side/mainHTTP/server.py
+++ src/rpi4-side/mainHTTP/server.py
@@ -68,6 +68,7 @@ class StreamingHandler(SimpleHTTPRequestHandler):
             super().do_GET()
     
     def do_POST(self):
+        # Calls global variables
         global class_predicted, stop_condition, fast_stop
         # Finds length of client's data
         length = int(self.headers['Content-Length'])
@@ -97,41 +98,62 @@ class ThreadingServer(ThreadingHTTPServer):
         super().__init__(*args)
 
     def serve_forever(self):
+        # Calls and initializes global variables
         global class_predicted, stop_condition, fast_stop
         class_predicted = 0
         fast_stop = 0
+        
         while True:
+            # Handles single request
             self.handle_request()
-            
             sleep(0.01)
+
+            # Checks if the paddle needs to be stopped
             if fast_stop == 1:
+                # Send fast stop command
                 self.serial.write(('9\n').encode())
                 print('PADDLE BLOCKED')
             
             else:
+                # Checks if new predictions should be received and a class is predicted
                 if stop_condition == 0 and class_predicted != 0:
+                    # Sends the class recognized
                     self.serial.write((str(class_predicted) + '\n').encode())
                     print('DATA SENT')
+                    # Stops new predictions
                     stop_condition = 1
             
             sleep(0.01)
+
             if self.serial.in_waiting > 0:
-                #print("ENTERED CONDITION")
+                # Decodes the received data
                 received_data = self.serial.readline().decode('utf-8').strip()
-                #print(f"Received: {received_data}")
+                
+                # If the Arduino said so, start getting new predictions
                 if received_data == "42":
                     stop_condition = 0
+
+
+"""
+Function to handle streaming from a picamera while setting up a server for video trasmission.
+It also manages serial USB communication with an Arduino
+"""
 def stream():
+    # Calls and initializes global variables
     global class_predicted, stop_condition
     stop_condition = 0
+
+    # Initialize serial communication with the Arduino
     ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1.0) # controlla il timeout
     sleep(2)
+    # Clear any leftover data in the input buffer
     ser.reset_input_buffer()
     
+    # Check if the serial communication is open
     if ser.isOpen():
+        # Initialize picamera
         with picamera.PiCamera(framerate=20) as camera:
             camera.resolution = (704, 512)
-
             # Create and stream buffer
             frame_buffer = FrameBuffer()
             camera.start_recording(frame_buffer, format='mjpeg')
@@ -139,9 +161,12 @@ def stream():
             # Run server
             try:
                 address = ('', 8000)
+                # Define a handler to manage incoming streaming requests
                 handler = lambda *args: StreamingHandler(frame_buffer, *args)
-
+                
+                # Start a multithreaded server to handle requests and serial communication
                 server = ThreadingServer(ser, address, handler)
+                # Keep the server running indefinitely
                 server.serve_forever()
                 
             finally:
