62ba0395e1b22081de59b0955b189f08f98b399f
Gyzmoooo <giuliogismondi06@gmail.com>
2024-11-22 14:54:24 +0100

    Passing file to mac for better debug

--- src/rpi4-side/display/display_ip.py
+++ src/rpi4-side/display/display_ip.py
@@ -5,6 +5,21 @@ import subprocess
 # Initialize the LCD
 lcd = CharLCD('PCF8574', 0x27)
 
+def clear_display(row):
+  lcd.cursor_pos = (row, 0)
+  lcd.write_string(" " * 16)
+  lcd.cursor_pos = (row, 0)
+
+def get_ip_address():
+  try:
+    ip = subprocess.check_output("hostname -I", shell=True).decode("utf-8").strip.split()[0]
+    if(ip):
+      return ip
+    else:
+      return "No IP Found"
+  except Exception as e:
+    return "No IP Found"
+
 # Clear display
 lcd.clear()
 
@@ -14,10 +29,11 @@ lcd.write_string("IP Address:")
 
 # Fetch and display IP
 try:
-  ip = subprocess.check_output("hostname -I", shell=True).decode("utf-8").strip().split()[0]
-  #ip = socket.gethostbyname(socket.gethostname())
-  lcd.cursor_pos = (1, 0)
-  lcd.write_string(ip)
-except Exception as e:
-  lcd.cursor_pos = (1, 0)
-  lcd.write_string("No IP Found")
+  while True:
+    ip = get_ip_address()
+    clear_display(1)
+    lcd.write_string(ip)
+    sleep(60)
+
+except KeyboardInterrupt:
+  lcd.clear()
