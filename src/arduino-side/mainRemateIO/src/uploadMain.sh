ba5f223ab20dfc967a7d6001d3d9ca7820505f07
PabloVro006 <vairop005@gmail.com>
2024-11-24 14:00:44 +0100

    Remove of MakeFile and upload.sh

--- src/arduino-side/mainRemateIO/src/uploadMain.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-folder=build-uno
-[ -d "$folder" ] && rm -r "$folder"
-make
-/usr/share/arduino/hardware/tools/avr/bin/avrdude -q -V -p atmega328p -C /etc/avrdude.conf -D -c arduino -b 115200 -P /dev/ttyACM* -U flash:w:build-uno/main_arduino_.hex:i
