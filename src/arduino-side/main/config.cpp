5a457ac5ab28e20769d909046a7a917c047dc4b9
PabloVro006 <vairop005@gmail.com>
2024-11-25 13:12:06 +0100

    After throwing paper the cross does not go back

--- src/arduino-side/main/config.cpp
+++ src/arduino-side/main/config.cpp
@@ -173,8 +173,11 @@ void throwPaper(){
     resetMotorOffset(DISK, COUNTER_CLOCKWISE, diskOffsetDelay);  // Adjusting the disk's offset
     rotateMotor(CROSS, COUNTER_CLOCKWISE, 1);  // Now the cross rotates again to dispose also the second-arrived waste
     delay(serialDelay);
+    rotateMotor(DISK, COUNTER_CLOCKWISE, 1);
+    /*
     rotateMotor(CROSS, CLOCKWISE, 1);  // Once both the new waste and the previous waste have been thrown away, the cross starts to go back in place
     rotateMotorSIM(COUNTER_CLOCKWISE, CLOCKWISE); // Simultaneously rotates both the disk's motor and the cross's motor
+    */
     resetMotorOffset(CROSS, COUNTER_CLOCKWISE, crossOffsetDelay);  // Adjusting the cross's offset
     resetMotorOffset(DISK, CLOCKWISE, diskOffsetDelay);  // Adjusting the disk's offset
     paperAlreadyPresent = false;  // Now this becomes true because both wastes have been disposed
